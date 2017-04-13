766eef0a7c11c6a36d5a8f732c943588ae750641
Alexander <alxndr+github@gmail.com>
2017-04-13 00:23:49 -0400

    look for song title + date in the DOM
    on phish.in and relisten.net and archive.org
    
    ...also extract some functions

--- mp3extractor.js
+++ mp3extractor.js
@@ -1,41 +1,87 @@
 // to make a bookmarklet out of this:
 // copy all of this code and paste it into a JavaScript compressor like http://jscompress.com
 // then put "javascript:" in front of what it gives you, and save that as the URL for a bookmark.
-alert(function() {
-  var copyUrl = function(url) {
-    var d = document;
-    var textarea = d.createElement("textarea");
-    textarea.textContent = "[audio]" + url + "[/audio]";
-    d.body.appendChild(textarea);
-    var selection = d.getSelection();
-    selection.removeAllRanges();
-    textarea.select();
-    d.execCommand("copy");
-    selection.removeAllRanges();
-    d.body.removeChild(textarea);
-    return true;
-  };
-  var determineUrl = function() {
-    var w = window;
-    if (w.App) {
-      if (App.Player && App.Player.sm_sound) // phish.in
-        return "http://phish.in" + App.Player.sm_sound.url;
-      if (App.player && App.player.sound) // relisten.net
-        return App.player.sound.url;
-    }
-    if (w.jwplayer) { // archive.org
-      var j = jwplayer("jw6");
-      return "http://archive.org" + j.getPlaylist()[j.getPlaylistIndex()].file;
-    }
-    var soundcloudMetatag = document.querySelector("meta[content*='soundcloud://sounds:']");
-    if (soundcloudMetatag && soundcloudMetatag.content) { // soundcloud song pages only
-      var trackId = /\d+/.exec(soundcloudMetatag.content)[0];
-      return "https://api.soundcloud.com/tracks/"+trackId+"/stream?client_id=CLIENT_ID_GOES_HERE";
-    }
+var REGEX_ON_YYYY_MM_DD = /on (\d{4}-\d{2}-\d{2})$/;
+function elemById(id) {
+  return document.getElementById(id);
+}
+function elemsByClassName(className) {
+  return document.getElementsByClassName(className);
+}
+function elemsByTag(tagName) {
+  return document.getElementsByTagName(tagName);
+}
+function makeAudioTag(info) {
+  if (!info) {
     return false;
-  };
-  var url = determineUrl();
-  if (url && copyUrl(url))
+  }
+  var audioTag = "[audio]" + info.url + "[/audio]";
+  if (info.title) {
+    return "[b]" + (info.date ? info.date + " " : "") + info.title + "[/b] " + audioTag;
+  }
+  return audioTag;
+}
+function copyString(string) {
+  if (!string) {
+    return false;
+  }
+  var d = document;
+  var textarea = d.createElement("textarea");
+  textarea.textContent = string;
+  d.body.appendChild(textarea);
+  var selection = d.getSelection();
+  selection.removeAllRanges();
+  textarea.select();
+  d.execCommand("copy");
+  selection.removeAllRanges();
+  d.body.removeChild(textarea);
+  return true;
+}
+function extractTrackInfo(w) {
+  if (w.App) {
+    if (w.App.Player && w.App.Player.sm_sound) {
+      // phish.in
+      return {
+        date: elemsByClassName("show_date")[0].innerText,
+        url: "http://phish.in" + w.App.Player.sm_sound.url,
+        title: elemById("player_title").innerText
+      };
+    }
+    if (w.App.player && w.App.player.sound) {
+      // relisten.net
+      var dateMatch = elemsByClassName("album")[0].innerText.match(REGEX_ON_YYYY_MM_DD);
+      return {
+        date: dateMatch && dateMatch[1],
+        url: w.App.player.sound.url,
+        title: elemsByClassName("title")[0].innerText
+      };
+    }
+  }
+  if (w.jwplayer) {
+    // archive.org
+    var player = w.jwplayer("jw6");
+    var track = player.getPlaylist()[player.getPlaylistIndex()];
+    var dateMatch = elemsByTag("h1")[0].innerText.match(REGEX_ON_YYYY_MM_DD);
+    return {
+      date: dateMatch && dateMatch[1],
+      url: "http://archive.org" + track.file,
+      title: track.title.replace(/\d+\. /, "")
+    };
+  }
+  var soundcloudMetatag = w.document.querySelector("meta[content*='soundcloud://sounds:']");
+  if (soundcloudMetatag && soundcloudMetatag.content) {
+    // soundcloud song pages only
+    var trackId = /\d+/.exec(soundcloudMetatag.content)[0];
+    return {
+      url: "https://api.soundcloud.com/tracks/"+trackId+"/stream?client_id=CLIENT_ID_GOES_HERE"
+    };
+  }
+  return false;
+}
+alert(function() {
+  var info = extractTrackInfo(window);
+  if (info && copyString(makeAudioTag(info))) {
     return "Copied audio tag to clipboard!";
+  }
   return "Could not determine URL of MP3...";
 }());
